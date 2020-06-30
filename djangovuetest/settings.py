4702074205961caff0b200a1a54096bee9185f1f
qw-jl <joseluis@quitiweb.com>
2020-06-30 15:53:37 +0200

    Eliminado pycache

--- djangovuetest/settings.py
+++ djangovuetest/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'djangovuetest.notes'
+    'djangovuetest.notes',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
